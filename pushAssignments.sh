70c4f650b8d8e2baf28c1ed40aea1641de7cda0a
uni20141379 <rugyoon@gmail.com>
2019-03-22 10:38:54 +0900

    ADD function to check directory

--- pushAssignments.sh
+++ pushAssignments.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+function check_dir() {
+    if [ ! -d $1 ] ; then
+        echo "$1 : No such directory."
+        exit 1;
+    fi
+}
+
 StudentFile=$1
 
-REPODIR=""
+group=""  # gitlab group is a course
+students_dir=""  # directory of all students project
+check_dir ${students_dir}
+
+assignment=""
+check_dir ${assignment}
 
 prefix="uni"
 
 while IFS= read -r username 
 do
-    sId=${username#${prefix}}
-    echo ${sId}
+    echo ${username}
 
-    cd ${REPODIR}
-    git clone 'Git URL as SSH' #Please make sure to activate the deployed key, use SSH not HTTP from git repository
-    cd 'Change Directory to student directory'
+    cd ${students_dir}
+    git clone 'git@gitlab.com:'${group}'/'${username}'.git' # use SSH not HTTP from git repository
+    cd ${username}
+    check_dir ${username}  # check if git clone works
 
-    #git pull origin master # Because of some students may have modified something
+    git pull origin master # Because some students may have modified something
 
-    mkdir sp_assignment3
-    cd sp_assignment3
-    #copy assignment files to the repo
+    cp -r ${assignment} . # copy assignment files to the repo
 
     git add ./
-    git commit -m \"$2\"
+    git commit -m "ADD ${assignment}"
     git push origin master
 done < ${StudentFile}
